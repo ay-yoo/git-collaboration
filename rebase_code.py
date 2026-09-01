cbc8e7f165451adef9d06c88cc8ef3ca84ae07bb
ay-yoo <Aileen050925@gmail.com>
2026-09-01 11:18:14 -0400

    main commit

--- rebase_code.py
+++ rebase_code.py
@@ -1,6 +1,7 @@
-def say_goodbye(name) -> None:
-    print("Goodbye ", name)
+def say_goodbye(name,number) -> None:
+    for i in range(number):
+        print("Goodbye ", name)
 
 
 if __name__ == "__main__":
-    say_goodbye("Walter")
+    say_goodbye("Walter", 3)
